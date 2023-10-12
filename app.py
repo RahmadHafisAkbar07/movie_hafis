3132b4b56e126e839a0894d88bd461b0142a8c6e
Rahmad Hafis Akbar <rahmad.hafis.akbar07@gmail.com>
2023-10-12 10:19:56 +0700

    update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ def movie_post():
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
-    return jsonify({'msg': 'Permintaan GET!'})
+    movie_list = list(db.movies.find({},{'_id':False}))
+    return jsonify({'movies':movie_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
